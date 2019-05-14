6e7c5b00595788bb0e25d6af9ed88d69cbe7da9b
ryanwolhuter <dev@ryanwolhuter.com>
2019-05-14 12:33:04 +0200

    Add JS processing with watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
 function copy() {
   return gulp.src([
     'app/*.html',
     'app/**/*.jpg',
     'app/**/*.css',
-    'app/**/*.js',
   ])
     .pipe(gulp.dest('build'));
 }
@@ -20,5 +22,25 @@ function serve() {
     port: 3000
   });
 }
+
+function processJs() {
+  return gulp.src('app/scripts/*.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('build/scripts'));
+}
+
+gulp.task('processJs', processJs);
+
+function watch() {
+  gulp.watch('app/scripts/*.js', processJs);
+}
+
+gulp.task('watch', watch);
 
 gulp.task('buildAndServe', gulp.series(copy, serve));
